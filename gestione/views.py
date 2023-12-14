0474cfc8a5026a1033220299b4446ab0d6a449ed
amndrea <amndrea30@gmail.com>
2023-12-14 09:41:21 +0100

    first commit

--- gestione/views.py
+++ gestione/views.py
@@ -9,37 +9,48 @@ HALFURL = "http://10.1.1.109:8001/v1/"
 # ************************************************************************ #
 
 
-
+# ********************************************************************************** #
 # ********************************************************************************** #
 #                     SUPPORT METHODS FOR VIEWS
 # ********************************************************************************** #
+# ********************************************************************************** #
+
 
 # ----------------------------------------------------------------------- #
 # Function that returns all the ingested documents in JSON format
 # ----------------------------------------------------------------------- #
 def json_documenti():
-    URL = HALFURL + "ingest/list"
+    url = HALFURL + "ingest/list"
     try:
-        response = requests.get(URL)
+        response = requests.get(url)
         response.raise_for_status()
         data = response.json()
         return data
     except Exception as e:
         print(e)
 
+
+# ********************************************************************************** #
+# ********************************************************************************** #
+#                                API FOR PRIVATEGPT
+# ********************************************************************************** #
+# ********************************************************************************** #
+
+
 # ----------------------------------------------------------------------- #
 # Function that show the actual health of E-ListaGPT server
 # ----------------------------------------------------------------------- #
 def health(request):
-    URL = "http://10.1.1.109:8001/health"
+    url = "http://10.1.1.109:8001/health"
     headers = {'Accept': 'application/json'}
-    response = requests.get(URL, headers=headers)
-    print(response)
-    return render(request, 'gestione/health.html')
+    response = requests.get(url, headers=headers)
+    status = response.status_code
+    return render(request, template_name='gestione/health.html', context={'status': status})
+
 
 # ----------------------------------------------------------------------- #
 # Function that given all the ingested documents in JSON format,
-# show it in html
+# show it in html template
 # ----------------------------------------------------------------------- #
 def ingest_list(request):
     data = json_documenti()
@@ -48,6 +59,3 @@ def ingest_list(request):
     nomi_file = set(doc["file_name"] for doc in doc_info_list)
     context = {"documenti": nomi_file}
     return render(request, template_name="gestione/list_ingest.html", context=context)
-
-
-
