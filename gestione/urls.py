330f0edf854dd8e61fe0ac47561ed99b16d2fbff
amndrea <amndrea30@gmail.com>
2023-12-14 12:22:40 +0100

    first commit

--- gestione/urls.py
+++ gestione/urls.py
@@ -5,8 +5,10 @@ from .views import *
 app_name = "gestione"
 
 urlpatterns = [
+
+    # URL at which I view the server's health status
+    path('health/', health, name="health"),
     # URL to which I show the documents currently ingested
     path('list_ingest/', ingest_list, name="list_ingest"),
 
-    path('health/', health, name="health")
 ]
